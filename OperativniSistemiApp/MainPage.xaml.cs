375787da432166ac88d474796c2b19d9308b3194
ivana_ma <ivamarkovic1996@gmail.com>
2021-04-08 16:24:54 +0200

    Initial commit

---
+++ OperativniSistemiApp/MainPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using OperativniSistemiApp.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace OperativniSistemiApp
+{
+    public sealed partial class MainPage : Page
+    {
+        SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultText.Text == "Done.")
+            {
+                await semaphore.WaitAsync();
+                try
+                {
+                    coolButton.IsEnabled = false;
+                    string text = "Not done.";
+                    await Task.Run(() =>
+                    {
+                        Task.Delay(5000).GetAwaiter().GetResult();
+                        text = "Really not done.";
+                    });
+                    resultText.Text = text;
+                    coolButton.IsEnabled = true;
+                }
+                finally
+                {
+                    semaphore?.Release();
+                }
+            }
+            else
+                resultText.Text = "Done.";
+        }
+
+        private async void BadButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                for (int i = 0; i <= 10; ++i)
+                {
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => progressBar.Value = i);
+                    Task.Delay(1000).GetAwaiter().GetResult();
+                }
+            });
+        }
+
+        private async void ListButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                CustomControl progressBar = null;
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                        () =>
+                        {
+                            progressBar = new CustomControl() { ProgressValue = 10, Margin = new Thickness(5) };
+                            stackPanel.Children.Add(progressBar);
+                        });
+                for (int i = 0; i <= 10; ++i)
+                {
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => progressBar.ProgressValue = i);
+                    await Task.Delay(1000);
+                }
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => stackPanel.Children.Remove(progressBar));
+            });
+        }
+
+        private async void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            downloadButton.IsEnabled = false;
+
+            var savePicker = new Windows.Storage.Pickers.FileSavePicker
+            {
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads,
+                SuggestedFileName = "New Document"
+            };
+            savePicker.FileTypeChoices.Add("HTML", new List<string>() { ".htm" });
+            StorageFile storageFile = await savePicker.PickSaveFileAsync();
+            if (storageFile == null)
+            {
+                downloadButton.IsEnabled = true;
+                return;
+            }
+
+            progressRing.Visibility = Visibility.Visible;
+            string html;
+            using (HttpClient client = new HttpClient())
+                html = await client.GetStringAsync(urlText.Text);
+            progressRing.Visibility = Visibility.Collapsed;
+
+
+            await FileIO.WriteTextAsync(storageFile, html);
+            //File.WriteAllText("C:\\test.html", html, System.Text.Encoding.UTF8);
+            MessageDialog dialog = new MessageDialog("Your file was saved.", "Success.");
+            await dialog.ShowAsync();
+
+            downloadButton.IsEnabled = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(SecondExample));
+        }
+    }
+}
---
+++ OperativniSistemiApp/SecondExample.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace OperativniSistemiApp
+{
+    public sealed partial class SecondExample : Page
+    {
+        const int NumValues = 5;
+        StackPanel[] panels;
+        int[] values;
+        SemaphoreSlim[] semaphores = new SemaphoreSlim[]
+        {
+            new SemaphoreSlim(1),
+            new SemaphoreSlim(1),
+            new SemaphoreSlim(1)
+        };
+        //SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+        public SecondExample()
+        {
+            this.InitializeComponent();
+            panels = new StackPanel[] { panel1, panel2, panel3 };
+            values = new int[panels.Length];
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < NumValues; ++i)
+            {
+                /*await semaphore.WaitAsync();
+                try
+                {*/
+                //for (int j = 0; j < panels.Length; ++j)
+                Parallel.For(0, panels.Length, async j =>
+                {
+                    await semaphores[j].WaitAsync();
+                    try
+                    {
+                        int value = values[j];
+                        //await Task.Delay(500);
+                        await Task.Run(() => LongExecution());
+                        values[j] = ++value;
+                        await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                            panels[j].Children.Add(new TextBlock() { Text = values[j].ToString() })
+                        );
+                    }
+                    finally
+                    {
+                        semaphores[j].Release();
+                    }
+                });
+                /*}
+                finally
+                {
+                    semaphore.Release();
+                }*/
+            }
+        }
+
+        private void LongExecution()
+        {
+            Task.Delay(500).GetAwaiter().GetResult();
+        }
+    }
+}
